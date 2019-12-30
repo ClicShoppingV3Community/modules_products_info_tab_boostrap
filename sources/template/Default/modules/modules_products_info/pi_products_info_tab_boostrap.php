344c70bd36acb975c4d1e00cbc0b87f608fa0de9
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-12-30 14:20:19 -0500

    small update

--- sources/template/Default/modules/modules_products_info/pi_products_info_tab_boostrap.php
+++ sources/template/Default/modules/modules_products_info/pi_products_info_tab_boostrap.php
@@ -119,7 +119,7 @@
       $CLICSHOPPING_Db = Registry::get('Db');
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want activate this module ?',
+          'configuration_title' => 'Do you want to enable this module ?',
           'configuration_key' => 'MODULE_PRODUCTS_INFO_DESCRIPTION_TAB_BOOSTRAP_STATUS',
           'configuration_value' => 'True',
           'configuration_description' => 'Note : Use this syntax to create your tab description without the " : "<" tabcatalog ">" "<" /tabcatalog ">"',
@@ -131,10 +131,10 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Veuillez selectionner la largeur de l\'affichage?',
+          'configuration_title' => 'Please select the width of the display?',
           'configuration_key' => 'MODULE_PRODUCTS_INFO_DESCRIPTION_TAB_BOOSTRAP_CONTENT_WIDTH',
           'configuration_value' => '12',
-          'configuration_description' => 'Veuillez indiquer un nombre compris entre 1 et 12',
+          'configuration_description' => 'Please enter a number between 1 and 12',
           'configuration_group_id' => '6',
           'sort_order' => '1',
           'set_function' => 'clic_cfg_set_content_module_width_pull_down',
@@ -143,10 +143,10 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Ordre de tri d\'affichage',
+          'configuration_title' => 'Sort order',
           'configuration_key' => 'MODULE_PRODUCTS_INFO_DESCRIPTION_TAB_BOOSTRAP_SORT_ORDER',
-          'configuration_value' => '100',
-          'configuration_description' => 'Ordre de tri pour l\'affichage (Le plus petit nombre est montré en premier)',
+          'configuration_value' => '131',
+          'configuration_description' => 'Sort order of display. Lowest is displayed first. The sort order must be different on every module',
           'configuration_group_id' => '6',
           'sort_order' => '3',
           'set_function' => '',
